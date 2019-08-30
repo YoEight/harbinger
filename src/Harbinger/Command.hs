7baa54cf0b9a507b0474167eaea5b811b92be024
YoEight <yo.eight@gmail.com>
2019-08-30 23:09:38 +0200

    Implement command parsing.

---
+++ src/Harbinger/Command.hs
@@ -0,0 +1,93 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module    :  Harbinger.Command
+-- Copyright :  (C) 2019 Yorick Laupa
+-- License   :  (see the file LICENSE)
+-- Maintainer:  Yorick Laupa <yo.eight@gmail.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+--------------------------------------------------------------------------------
+module Harbinger.Command where
+
+--------------------------------------------------------------------------------
+import Data.Text (Text)
+import Options.Applicative
+import Safe (readMay)
+
+--------------------------------------------------------------------------------
+data Args =
+  Args
+  { argsHost    :: String
+  , argsPort    :: Int
+  , argsCommand :: Command
+  } deriving Show
+
+--------------------------------------------------------------------------------
+data Command
+  = CheckConnection
+  deriving Show
+
+--------------------------------------------------------------------------------
+getArgs :: IO Args
+getArgs = execParser go
+  where
+    go = info (helper <*> parseArgs) description
+
+    description =
+      fullDesc <> header "Harbinger - An EventStore administration tool."
+
+--------------------------------------------------------------------------------
+parseArgs :: Parser Args
+parseArgs =
+  Args <$> parseHost
+       <*> parseTcpPort
+       <*> parseCommand
+
+--------------------------------------------------------------------------------
+parseHost :: Parser String
+parseHost = strOption go
+  where
+    go = mconcat [ long "tcp-host"
+                 , metavar "HOST"
+                 , help "Database host."
+                 , value "localhost"
+                 , showDefault
+                 ]
+
+--------------------------------------------------------------------------------
+parseTcpPort :: Parser Int
+parseTcpPort = option (eitherReader check) go
+  where
+    go = mconcat [ long "tcp-port"
+                 , metavar "PORT"
+                 , help "Database TCP port."
+                 , value 1113
+                 , showDefault
+                 ]
+
+
+    check input =
+      case readMay input of
+        Nothing -> Left "Invalid HTTP port number"
+        Just port
+          | port >= 1 && port <= 65535 -> Right port
+          | otherwise -> Left $ "Port should be between [1,65535]"
+
+--------------------------------------------------------------------------------
+parseCommand :: Parser Command
+parseCommand =
+  subparser $
+    mconcat
+      [ command "check" $
+          go "Checks if a database node is reachable."
+            parseCheckConnectionCommand
+      ]
+  where
+    go desc parser =
+      info (helper <*> parser)
+           (progDesc desc)
+
+--------------------------------------------------------------------------------
+parseCheckConnectionCommand :: Parser Command
+parseCheckConnectionCommand = pure CheckConnection
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,21 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module    :  Main
+-- Copyright :  (C) 2019 Yorick Laupa
+-- License   :  (see the file LICENSE)
+-- Maintainer:  Yorick Laupa <yo.eight@gmail.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+--------------------------------------------------------------------------------
 module Main where
 
+--------------------------------------------------------------------------------
+import Prelude hiding (getArgs)
+
+--------------------------------------------------------------------------------
+import Harbinger.Command
+
+--------------------------------------------------------------------------------
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = getArgs >>= print
